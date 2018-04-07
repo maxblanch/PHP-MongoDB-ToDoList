ea5fc8469d298bb3d24ead950662480e8771fc1c
Maxime Blanchard <max@MacBook-Pro.local>
2018-04-07 15:21:16 -0400

    cookie page deleted

--- helpers/cookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if ($_POST['userLogin'] === 'admin' && $_POST['userPassword'] === 'admin') {
-        session_start();
-        $_SESSION['name'] = $_POST['userLogin'];
-        setcookie('admin', 'admin', time() + 60, '/');
-        header("location: ../index.php?controller=Home&action=home");
-    } else {
-        header("Location: ../index.php?controller=Signin&action=processSignin");
-    }
